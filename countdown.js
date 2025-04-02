e06b58b1e80dbda8b4617f4ec8c8de52b9d3408b
Felipe Baba <felipebabateixeira@hotmail.com>
2025-04-02 13:33:24 -0300

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,21 @@
+const targetDate = new Date("april 13, 2025 00:00:00").getTime();
+
+const interval = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById("timer").textContent = "Contagem regressiva finalizada!";
+    }
+}, 1000);
